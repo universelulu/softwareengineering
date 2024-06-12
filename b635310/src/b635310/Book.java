fff4d22c4e223aec239989bf2282cc55d9dd1044
jangs <jangs@DESKTOP-R46BTDF>
2024-06-13 00:01:47 +0900

    불필요한 주석 정리

--- b635310/src/b635310/Book.java
+++ b635310/src/b635310/Book.java
@@ -1,32 +1,31 @@
 package b635310;
 
-//고유 id와 제목을 가진 책을 나타낸다.
 
-//함수에서 쓰이는 클래스이다.
 public class Book {
 
-	private int id; //책 아이디(고유 식별자)
-    private String title; //책의 제목
+	private int id; 
+    private String title; 
 
-    //새로운 book 객체를 생성한다.
-    public Book(int id, String title) {
+    
+    public Book(int id, String title) // Book 클래스 생성자
+    {
         this.id = id;
         this.title = title;
     }
 
-    //아이디(고유 식별자)를 반환한다.
-    public int getId() {
+    public int getId() 
+    {
         return id;
     }
 
-    //책의 제목을 반환한다.
-    public String getTitle() {
+    public String getTitle() 
+    {
         return title;
     }
 
-    //책의 아이디, 제목을 return 한다.
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Book{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
