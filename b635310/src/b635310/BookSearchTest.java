f50562c809dc2787fb21c1d86228d04665d07f91
jangs <jangs@DESKTOP-R46BTDF>
2024-06-13 17:03:55 +0900

    2024-06-13(import 수정)

--- b635310/src/b635310/BookSearchTest.java
+++ b635310/src/b635310/BookSearchTest.java
@@ -1,8 +1,8 @@
 package b635310;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class BookSearchTest {
 
